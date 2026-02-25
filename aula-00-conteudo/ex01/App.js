3d00b42ab737e2aaec9f7bd99bcbb304e2fe14e8
lucasdomingueslvr <130410085+lucasdomingueslvr@users.noreply.github.com>
2026-02-25 00:22:52 +0000

    e

--- aula-00-conteudo/ex01/App.js
+++ aula-00-conteudo/ex01/App.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
+import { ScrollView, Text, StyleSheet, View } from "react-native";
 export default function App(){
-  return(
-    <View style={styles.container}>
-      <Text style={styles.text}>teste</Text>
-    </View>
-  );
+return (
+<ScrollView contentContainerStyle={styles.container}>
+{Array.from({ length: 20}).map((_, index) => (
+<View key={index} style={styles.item}>
+<Text style={styles.text}>Item {index + 1}</Text>
+</View>
+))}
+</ScrollView>
+);
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#aaa',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  }
-});
+container: {
+padding: 20,
+marginTop: 20,
+},
+item: {
+marginBottom: 10,
+padding: 20,
+backgroundColor: '#f0f0f0',
+borderRadius: 8,
+},
+text: {
+fontSize: 16,
+fontWeight: 'bold',
+},
+})
