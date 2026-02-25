cb96dd9f0b19d15b73e011f81ffcc7dfacda2c96
lucasdomingueslvr <130410085+lucasdomingueslvr@users.noreply.github.com>
2026-02-25 00:22:44 +0000

    EXs

---
+++ aula-00-conteudo/ex01/components/OlaMundo.js
@@ -0,0 +1,10 @@
+import { View, Text} from "react-native"
+
+export default function OlaMundo(){
+    return(
+        <View>
+            <Text>Olá</Text>
+            <Text>Mundo</Text>
+        </View>
+    )
+}
